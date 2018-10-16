f4d4e551cecd4728c1eb6b523c7e225b3d5c18f3
nd1226 <doanvannhan1226l@gmail.com>
2018-10-16 09:29:20 +0700

    Finish chapter 11

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,14 @@
 module UsersHelper
-  def gravatar_for user, options = {size: Settings.user.size}
+  def gravatar_for user
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
   def current_admin user
     return unless current_user.admin? && !current_user?(user)
+    link_to t("users.destroy.delete"), user, method: :delete,
+      data: {confirm: t("users.destroy.confirm")},
+      class: "btn btn-danger"
   end
 end
